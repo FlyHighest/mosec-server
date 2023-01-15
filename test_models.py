f242028251db2d34f5d2e9bdab4736b3b09fff9d
FlyHighest <zhangtianyu@buaa.edu.cn>
2023-01-15 17:58:39 +0800

    update

--- test_models.py
+++ test_models.py
@@ -3,16 +3,17 @@ import torch
 from diffusers import DiffusionPipeline
 from transformers import AutoTokenizer
 from params.constants import MODELS,MODEL_CACHE
+models = dict()
 for model_name in MODELS.keys():
     print("Load "+model_name)
-    p = DiffusionPipeline.from_pretrained(
+    models[model_name] = DiffusionPipeline.from_pretrained(
                                  MODELS[model_name],
                                  torch_dtype=torch.float16,
                                  cache_dir=MODEL_CACHE
-                             )
+                             ).to("cuda")
        
     if model_name == "Chinese-style-sd-2-v0.1":
-        p.tokenizer = AutoTokenizer.from_pretrained("lyua1225/clip-huge-zh-75k-steps-bs4096",cache_dir=MODEL_CACHE, trust_remote_code=True)
+        models[model_name].tokenizer = AutoTokenizer.from_pretrained("lyua1225/clip-huge-zh-75k-steps-bs4096",cache_dir=MODEL_CACHE, trust_remote_code=True)
     print(model_name +" OK")
 
 exit(0)
