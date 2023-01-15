cc292315abdb480ab9eaef4ca1d4a7d3c5743763
FlyHighest <zhangtianyu@buaa.edu.cn>
2023-01-15 17:54:07 +0800

    update

--- test_models.py
+++ test_models.py
@@ -1,14 +1,15 @@
 import os
 import torch
-from diffusers import StableDiffusionPipeline
-from diffusers.pipelines.stable_diffusion.safety_checker import (
-    StableDiffusionSafetyChecker,
-)
-from params.constants import MODEL_CACHE,MODELS
-
-SD_2_1 = "stabilityai/stable-diffusion-2-1"
-
-StableDiffusionPipeline.from_pretrained(
-    MODEL_CACHE+"/stable-diffusion-2-1",
-    torch_dtype=torch.float16
-)
+from diffusers import DiffusionPipeline
+from transformers import AutoTokenizer
+from params.constants import MODELS,MODEL_CACHE
+for model_name in MODELS.keys():
+    p = DiffusionPipeline.from_pretrained(
+                                 MODELS[model_name],
+                                 torch_dtype=torch.float16,
+                                 cache_dir=MODEL_CACHE
+                             )
+       
+    if model_name == "Chinese-style-sd-2-v0.1":
+        p.tokenizer = AutoTokenizer.from_pretrained("lyua1225/clip-huge-zh-75k-steps-bs4096",cache_dir=MODEL_CACHE, trust_remote_code=True)
+exit(0)
