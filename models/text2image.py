25649526fd5ee2667693833d0e2b457f1bbc76f3
FlyHighest <zhangtianyu@buaa.edu.cn>
2023-01-15 17:50:58 +0800

    update

--- models/text2image.py
+++ models/text2image.py
@@ -3,27 +3,32 @@ from params.constants import MODELS,MODEL_CACHE
 import torch
 from .scheduler import make_scheduler
 import traceback
-
+from transformers import AutoTokenizer
+from typing import Dict
 class Text2ImageModel:
     def __init__(self, device, worker_id) -> None:
         for model_name in MODELS.keys():
             print("Load model ",model_name)
-            self.__setattr__(model_name,
-                             DiffusionPipeline.from_pretrained(
+            self.models:Dict[str,DiffusionPipeline] = dict()
+
+            self.models[model_name]= DiffusionPipeline.from_pretrained(
                                  MODELS[model_name],
                                  torch_dtype=torch.float16,
                                  cache_dir=MODEL_CACHE
-                             ).to(device))
-            pipeline: DiffusionPipeline = self.__getattribute__(model_name)
-            # pipeline.safety_checker = self.AltDiffusion.safety_checker
-            pipeline.enable_xformers_memory_efficient_attention()
+                             ).to(device)
+            self.models[model_name].enable_xformers_memory_efficient_attention()
+            self.models[model_name].feature_extractor = self.models["Taiyi-Chinese-v0.1"].feature_extractor
+            self.models[model_name].safety_checker =  self.models["Taiyi-Chinese-v0.1"].safety_checker
+        
+        self.models["Chinese-style-sd-2-v0.1"].tokenizer = AutoTokenizer.from_pretrained("lyua1225/clip-huge-zh-75k-steps-bs4096",cache_dir=MODEL_CACHE, trust_remote_code=True)
+        
         self.worker_id = worker_id
         self.device = device
         self.output_name = f"/tmp/output_t2i_id{self.worker_id}.webp"
 
     def __call__(self, model_name, scheduler_name,seed, pipeline_params: dict):
         try:
-            pipeline = self.__getattribute__(model_name)
+            pipeline = self.models[model_name]
 
             pipeline.scheduler = make_scheduler(scheduler_name,model_name)
             with torch.inference_mode():
