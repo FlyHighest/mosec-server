cf4eba0c326ac85958cf502795015fb55cb7c6e3
FlyHighest <zhangtianyu@buaa.edu.cn>
2023-01-16 23:20:34 +0800

    set delete time

--- storage/storage_tool.py
+++ storage/storage_tool.py
@@ -1,6 +1,6 @@
 import sys 
 sys.path.append(".")
-from qiniu import Auth, put_file, etag
+from qiniu import Auth, put_file, etag, BucketManager
 from params.secret import qiniu_access_key_id,qiniu_access_key_secret,qiniu_public_url
 import nanoid 
 import traceback,time
@@ -10,6 +10,7 @@ IMAGE_ID_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 class StorageTool:
     def __init__(self) -> None:
         self.q = Auth(qiniu_access_key_id,qiniu_access_key_secret)
+        self.bucket = BucketManager(self.q)
   
     def upload(self,img_path):
         object_name = time.strftime("%Y-%m-%d")+"/"+nanoid.generate(IMAGE_ID_ALPHABET, size=25)+".webp"
@@ -17,6 +18,7 @@ class StorageTool:
             token = self.q.upload_token("imagedraw",object_name)
             ret, _ = put_file(token,object_name,img_path,version="v2")
             assert ret['hash'] == etag(img_path)
+            self.bucket.delete_after_days("imagedraw",object_name, '7')
             return qiniu_public_url+object_name
         except:
             traceback.print_exc()
