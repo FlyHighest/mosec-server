eb2743d312ef119300d36478c786f7cc691fea84
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-01-20 21:18:24 +0800

    change to webui

--- params/constants.py
+++ params/constants.py
@@ -4,6 +4,6 @@ MODEL_URL ={
     "ACertainThing":         "http://localhost:6003/sdapi/v1/txt2img",
     "OpenJourney":           "http://localhost:6004/sdapi/v1/txt2img",
     "Protogen-x5.8":         "http://localhost:6005/sdapi/v1/txt2img",
-    "Stable-Diffusion-v1.5": "http://localhost:6006/sdapi/v1/txt2img"
+    "Stable-Diffusion-v1.5": "http://localhost:6001/sdapi/v1/txt2img"
    }
 }
