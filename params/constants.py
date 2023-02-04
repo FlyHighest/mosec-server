9a9ed3fa896758dfd892f8837ad0991ceed7a0de
Terry <tyz.xyz@qq.com>
2023-02-04 11:10:46 +0800

    me

--- params/constants.py
+++ params/constants.py
@@ -4,6 +4,7 @@ MODEL_URL ={
     "ACertainThing":         "http://localhost:6003/sdapi/v1/txt2img",
     "OpenJourney":           "http://localhost:6004/sdapi/v1/txt2img",
     "Protogen-x5.8":         "http://localhost:6005/sdapi/v1/txt2img",
-    "Stable-Diffusion-v1.5": "http://localhost:6001/sdapi/v1/txt2img"
+    "Stable-Diffusion-v1.5": "http://localhost:6001/sdapi/v1/txt2img",
+    "RealisticVision-V1.3": "http://localhost:6000/sdapi/v1/txt2img"
    }
 }
