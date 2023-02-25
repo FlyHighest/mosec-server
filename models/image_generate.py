06702c408569b9788fb84ccc00d779f84c9113ab
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-02-25 22:57:50 +0800

    add img2img

--- models/image_generate.py
+++ models/image_generate.py
@@ -82,8 +82,7 @@ class ImageGenerationModel:
                 json_data.update({
                      "enable_hr": True,
                      "hr_scale":  2,
-                     "hr_upscaler": webuiapi.HiResUpscaler.Latent,
-                     "hr_second_pass_steps": 20,
+                     "hr_upscale": webuiapi.HiResUpscaler.Latent,
                      "denoising_strength":0.6
                 })
                 
