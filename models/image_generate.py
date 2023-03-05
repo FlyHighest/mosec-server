df3cd168f8b445455a15d575f64aa45a4ba91660
Terry <tyz.xyz@qq.com>
2023-03-05 14:30:43 +0800

    udate

--- models/image_generate.py
+++ models/image_generate.py
@@ -162,7 +162,7 @@ class ImageGenerationModel:
                      "enable_hr": True,
                      "hr_scale":  2,
                      "hr_upscaler": webuiapi.HiResUpscaler.Latent,
-                     "hr_second_pass_steps": 20,
+                     "hr_second_pass_steps": 10,
                      "denoising_strength":0.6
                 })
                 
