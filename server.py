f8276d1afbe5eb098a536dbef92eacd55f0b304e
Terry Zhang <zhangtianyu@buaa.edu.cn>
2023-05-13 14:59:37 +0800

    private access

--- server.py
+++ server.py
@@ -95,7 +95,7 @@ class Inference(Worker):
         super().__init__()
         # initialization
         torch.backends.cudnn.benchmark = True
-        worker_id = self.worker_id - 1
+        worker_id = self.worker_id
         self.device = torch.device("cuda:"+str(worker_id))
         logger.info("using worker_id "+str(worker_id))
 
